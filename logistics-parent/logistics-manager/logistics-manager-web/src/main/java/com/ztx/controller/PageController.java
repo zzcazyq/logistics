a695af4e233307a2b38a9638a065d73d2a0b72ce
zzc <2453340304@qq.com>
2023-07-12 15:44:16 +0800

    整合ssm，jsp

---
+++ logistics-parent/logistics-manager/logistics-manager-web/src/main/java/com/ztx/controller/PageController.java
@@ -0,0 +1,32 @@
+package com.ztx.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 页面跳转的控制器
+ */
+@Controller
+public class PageController {
+
+    /**
+     * HOME页面
+     * @return
+     */
+    @RequestMapping(value = {"/","/home"})
+    public String showMain(){
+        return "main";
+    }
+
+    /**
+     * RestFul风格处理
+     * @return
+     */
+    @RequestMapping("/{path}")
+    public String showPage(@PathVariable String path){
+        return path;
+    }
+}
+
